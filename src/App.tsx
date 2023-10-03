6c26e075697675a9f16f295a97ae3e932c53b179
prushton2 <peterrushton418@gmail.com>
2023-10-03 17:45:30 -0400

    added icon and name

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  document.title = "prushton.com";
+
   return (
     <div className="App">
       <header className="App-header">
